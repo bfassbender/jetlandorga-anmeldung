9e64253f3c2ccc11baa736dbb263fd7d5e5ef2ad
Bernd Knobloch <serrin@posteo.de>
2015-06-10 08:52:49 +0200

    Favicons und metatags in index.php

--- index.php
+++ index.php
@@ -2,6 +2,30 @@
 <html lang="de">
 	<head  prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
 		
+		<meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Jetland 11 - Die eherne Feste - präsentiert von der Jetland Orga. Ein Fantasy Live Action Roleplay Con vom 13.05. - 16.05.2016">
+		<meta name="author" content="Jetland Orga">
+		<meta content="yes" name="apple-mobile-web-app-capable">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+		<title>Onlineanmeldung | Jetland 11 - Die eherne Feste | Jetland Orga</title>
+		
+		<!-- Favicon -->	
+		<link rel="icon" href="/img/favicon/favicon.ico" type="image/x-icon">
+		<link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="/img/favicon/favicon.ico" type="image/x-icon">
+		<link rel="apple-touch-icon" sizes="57x57" href="/img/favicon/favicon_57.png">
+		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/favicon/favicon_57.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/favicon_72.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/favicon_114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/favicon_120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/img/favicon/favicon_144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/img/favicon/favicon_152.png">
+		<meta name="application-name" content="Jetland 11">
+		<meta name="msapplication-TileImage" content="/img/favicon/favicon_favicon-144.png">
+		<meta name="msapplication-TileColor" content="#666666">		
 		
 		<!-- Stylesheets -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
