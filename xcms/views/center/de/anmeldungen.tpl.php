06f2bc188355b9d234d075435ec9bf12fd041e89
Bernd Knobloch <serrin@posteo.de>
2015-06-12 22:07:29 +0200

    Hütten-NSC sind jetzt normale und Textfarbe in den Feldern ist wieder schwarz

--- xcms/views/center/de/anmeldungen.tpl.php
+++ xcms/views/center/de/anmeldungen.tpl.php
@@ -46,7 +46,7 @@ $nscBan = ($nscRest <= '0') ? true : false;
 				$('#boxnsc').show('fast');
 				$('#boxsc').hide('fast');
 				if($('#nsc_1').is(':checked')) {
-					$('#bar_art').html(' NSC Huette');
+					$('#bar_art').html(' NSC');
 				}
 				$('#bar_art').css('font-weight', 'bold');
 			} else if($('#sc').is(':checked')){
@@ -137,54 +137,54 @@ $nscBan = ($nscRest <= '0') ? true : false;
 		<h3>Angaben zum Spieler</h3>
 			<form class="form-horizontal" action='<? echo $_SERVER['PHP_SELF']?>?action=anmelden' method='POST' enctype='multipart/form-data' name="anmeldung"  onsubmit="return checkSubmitForm();">
 <?php if($scBan == true): ?>
-				<input type='hidden' name='member[warteliste]' value='1' />
+				<input class="form-control" type='hidden' name='member[warteliste]' value='1' />
 <?php endif; ?>		
 			<div class="form-group">
 				<label class="col-sm-3 control-label" for="vorname">Vorname<span style='color: red'>*</span></label>
 				<div class="col-sm-9">
-					<input id='vorname' type='text' name='member[vorname]' value='' size='30' onkeypress="return noreturn(event);" />
+					<input class="form-control" id='vorname' type='text' name='member[vorname]' value='' size='30' onkeypress="return noreturn(event);" />
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="col-sm-3 control-label" for="nachname">Nachname<span style='color: red'>*</span></label>
 				<div class="col-sm-9">
-					<input id='nachname' type='text' name='member[nachname]' value='' size='30' onkeypress="return noreturn(event);"/>
+					<input class="form-control" id='nachname' type='text' name='member[nachname]' value='' size='30' onkeypress="return noreturn(event);"/>
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="col-sm-3 control-label" for="strasse">Strasse<span style='color: red'>*</span></label>
 				<div class="col-sm-9">
-					<input id='strasse' type='text' name='member[strasse]' value='' size='30' onkeypress="return noreturn(event);"/>
+					<input class="form-control" id='strasse' type='text' name='member[strasse]' value='' size='30' onkeypress="return noreturn(event);"/>
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="col-sm-3 control-label" for="plz">Postleitzahl<span style='color: red'>*</span></label>
 				<div class="col-sm-9">
-					<input id='plz' type='text' name='member[plz]' value='' size='5' maxlength='5' onkeypress="return noreturn(event);"/>
+					<input class="form-control" id='plz' type='text' name='member[plz]' value='' size='5' maxlength='5' onkeypress="return noreturn(event);"/>
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="col-sm-3 control-label" for="ort">Ort<span style='color: red'>*</span></label>
 				<div class="col-sm-9">
-					<input id='ort' type='text' name='member[ort]' value='<? echo $_SESSION['c5']['member']['ort']?>' size='30' onkeypress="return noreturn(event);"/>
+					<input class="form-control" id='ort' type='text' name='member[ort]' value='<? echo $_SESSION['c5']['member']['ort']?>' size='30' onkeypress="return noreturn(event);"/>
 				</div>
 			</div>			
 			<div class="form-group">
 				<label class="col-sm-3 control-label" for="ort">Telefon</label>
 				<div class="col-sm-9">
-					<input id='telefon' type='text' name='member[telefon]' value='<? echo $_SESSION['c5']['member']['telefon']?>' size='30' onkeypress="return noreturn(event);"/>
+					<input class="form-control" id='telefon' type='text' name='member[telefon]' value='<? echo $_SESSION['c5']['member']['telefon']?>' size='30' onkeypress="return noreturn(event);"/>
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="col-sm-3 control-label" for="geb_datum">Geburtsdatum<span style='color: red'>*</span></label>
 				<div class="col-sm-9">
-					<input id='geb_datum' type='text' name='member[geb_datum]' value='<? echo $_SESSION['c5']['member']['geb_datum']?>' size='10' maxlength='10' onkeypress="return noreturn(event);" />
+					<input class="form-control" id='geb_datum' type='text' name='member[geb_datum]' value='<? echo $_SESSION['c5']['member']['geb_datum']?>' size='10' maxlength='10' onkeypress="return noreturn(event);" />
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="col-sm-3 control-label" for="email">E-Mail<span style='color: red'>*</span></label>
 				<div class="col-sm-9">
-					<input id='email' type='text' name='member[email]' value='<? echo $_SESSION['c5']['member']['email']?>' size='30' onkeypress="return noreturn(event);">
+					<input class="form-control" id='email' type='text' name='member[email]' value='<? echo $_SESSION['c5']['member']['email']?>' size='30' onkeypress="return noreturn(event);">
 				</div>
 			</div>
 			<div class="form-group">
@@ -192,10 +192,10 @@ $nscBan = ($nscRest <= '0') ? true : false;
 				<label class="col-sm-3 control-label">Ich helfe beim:</label>
 				<div class="col-sm-9 checkbox">
 				    <label>
-						<input type='checkbox' name='member[aufbau]' value='1' class='radio'>Aufbau
+						<input class="form-control" type='checkbox' name='member[aufbau]' value='1' class='radio'>Aufbau
 					</label><br />
 				    <label>
-						<input type='checkbox' name='member[abbau]' value='1' class='radio'>Abbau
+						<input class="form-control" type='checkbox' name='member[abbau]' value='1' class='radio'>Abbau
 					</label>				
 				</div>
 			</div>
@@ -203,13 +203,13 @@ $nscBan = ($nscRest <= '0') ? true : false;
 				<label class="col-sm-3 control-label">Larperfahrung</label>
 				<div class="col-sm-9 radio">
 				    <label>
-						<input type='radio' name='member[erfahrung]' checked="checked" value='0' class='radio'>Nein
+						<input class="form-control" type='radio' name='member[erfahrung]' checked="checked" value='0' class='radio'>Nein
 					</label><br />
 				    <label>
-						<input type='radio' name='member[erfahrung]' value='1' class='radio'>Ja &nbsp; (Con-Tage insgesamt)
+						<input class="form-control" type='radio' name='member[erfahrung]' value='1' class='radio'>Ja &nbsp; (Con-Tage insgesamt)
 					</label><br />
 				    <label>
-						<input type='text' name='member[erfahrung_tage]' value='' />
+						<input class="form-control" type='text' name='member[erfahrung_tage]' value='' />
 					</label>
 				</div>
 			</div>			
@@ -217,10 +217,10 @@ $nscBan = ($nscRest <= '0') ? true : false;
 				<label class="col-sm-3 control-label">Sanitäter</label>
 				<div class="col-sm-9 radio">
 					<label>
-						<input type='radio' name='member[sanitaeter]' checked="checked" value='0' class='radio'> Nein &nbsp;
+						<input class="form-control" type='radio' name='member[sanitaeter]' checked="checked" value='0' class='radio'> Nein &nbsp;
 					</label><br />
 					<label>
-						<input type='radio' name='member[sanitaeter]' value='1' class='radio'> Ja
+						<input class="form-control" type='radio' name='member[sanitaeter]' value='1' class='radio'> Ja
 					</label>
 				</div>
 			</div>
@@ -228,10 +228,10 @@ $nscBan = ($nscRest <= '0') ? true : false;
 				<label class="col-sm-3 control-label">Vegetarier</label>
 				<div class="col-sm-9 radio">
 					<label>
-						<input type='radio' name='member[vegetarier]' checked="checked" value='0' class='radio'> Nein &nbsp;
+						<input class="form-control" type='radio' name='member[vegetarier]' checked="checked" value='0' class='radio'> Nein &nbsp;
 					</label><br />
 					<label>
-						<input type='radio' name='member[vegetarier]' value='1' class='radio'> Ja
+						<input class="form-control" type='radio' name='member[vegetarier]' value='1' class='radio'> Ja
 					</label>
 				</div>	
 			</div>
@@ -239,13 +239,13 @@ $nscBan = ($nscRest <= '0') ? true : false;
 				<label class="col-sm-3 control-label">Krankheiten / Allergien</label>
 				<div class="col-sm-9 radio">
 					<label>
-						<input type='radio' name='member[krankheiten]' checked="checked" value='0' class='radio'> Nein &nbsp;
+						<input class="form-control" type='radio' name='member[krankheiten]' checked="checked" value='0' class='radio'> Nein &nbsp;
 					</label><br />
 					<label>
-						<input type='radio' name='member[krankheiten]' value='1' class='radio'> Ja, &nbsp;
+						<input class="form-control" type='radio' name='member[krankheiten]' value='1' class='radio'> Ja, &nbsp;
 					</label><br />
 				    <label>
-						<input type='text' name='member[krankheiten_welche]' value='' class='radio'>
+						<input class="form-control" type='text' name='member[krankheiten_welche]' value='' class='radio'>
 					</label>
 				</div>
 			</div>
@@ -253,10 +253,10 @@ $nscBan = ($nscRest <= '0') ? true : false;
 				<label class="col-sm-3 control-label">Ich muss durchschlafen</label>
 				<div class="col-sm-9 radio">
 					<label>
-						<input type='radio' name='member[durchschlafen]' checked="checked" value='0' class='radio'> Nein &nbsp;
+						<input class="form-control" type='radio' name='member[durchschlafen]' checked="checked" value='0' class='radio'> Nein &nbsp;
 					</label><br />
 					<label>					
-						<input type='radio' name='member[durchschlafen]' value='1' class='radio'> Ja
+						<input class="form-control" type='radio' name='member[durchschlafen]' value='1' class='radio'> Ja
 					</label><br />
 					<span class="small">(Nur Ja ankreuzen, wenn medizinische Gr&uuml;nde vorliegen)</span>
 				</div>
@@ -264,7 +264,7 @@ $nscBan = ($nscRest <= '0') ? true : false;
 			<div class="form-group">			
 				<label class="col-sm-3 control-label" for="zimmer">Ich m&ouml;chte mit folgenden Leuten in ein Zimmer:</label>
 				<div class="col-sm-9">
-					<input type='text' id='zimmer' name='member[zimmer]' value=''> 		
+					<input class="form-control" type='text' id='zimmer' name='member[zimmer]' value=''> 		
 				</div>
 			</div>
 			<h3>Teilnehmerart<span id='bar_art' style='text-weight: bold'><b>&nbsp;Spieler</b></span></h3>
@@ -272,8 +272,8 @@ $nscBan = ($nscRest <= '0') ? true : false;
 				<b>Ich komme als:</b><span style='color: red'> *</span>
 			</div>	
 			<div class='userinput'>
-				<input type='radio' name='member[rang]' value='sc' id="sc" checked="checked" class='radio' onclick="toggle()"> <b>Spieler</b> &nbsp;
-				<input type='radio' name='member[rang]' value='nsc|huette' id="nsc_1" class='radio' onclick="toggle()"> <b>NSC</b>
+				<input class="form-control" type='radio' name='member[rang]' value='sc' id="sc" checked="checked" class='radio' onclick="toggle()"> <b>Spieler</b> &nbsp;
+				<input class="form-control" type='radio' name='member[rang]' value='nsc|huette' id="nsc_1" class='radio' onclick="toggle()"> <b>NSC</b>
 			</div><br><br>
 		<div id="boxsc" style="display:block">
 			<div class='header'><b>Spieler</b></div>
@@ -281,28 +281,28 @@ $nscBan = ($nscRest <= '0') ? true : false;
 			<b> Angaben zum Charakter:</b><br /><br />
 			<div class='header' style='position: absolute'><b>Charaktername</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input id='charname' type='text' name='sc[charname]' size='30' onkeypress="return noreturn(event);">
+				<input class="form-control" id='charname' type='text' name='sc[charname]' size='30' onkeypress="return noreturn(event);">
 			</div><br>			
 			<div class='header' style='position: absolute'><b>Rasse</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input id='rasse' type='text' name='sc[rasse]' size='30' onkeypress="return noreturn(event);">
+				<input class="form-control" id='rasse' type='text' name='sc[rasse]' size='30' onkeypress="return noreturn(event);">
 			</div><br>
 			<div class='header' style='position: absolute'><b>Klasse</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input id='klasse' type='text' name='sc[klasse]' size='30' onkeypress="return noreturn(event);">
+				<input class="form-control" id='klasse' type='text' name='sc[klasse]' size='30' onkeypress="return noreturn(event);">
 			</div><br>
 			<div class='header' style='position: absolute'><b>Herkunft (Land)</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input id='herkunft' type='text' name='sc[herkunft]' size='30' onkeypress="return noreturn(event);">
+				<input class="form-control" id='herkunft' type='text' name='sc[herkunft]' size='30' onkeypress="return noreturn(event);">
 			</div><br>	
 			<div class='header' style='position: absolute'><b>Zauberkundig</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input type='radio' name='sc[zauber][]' value="0" checked="checked" size='30' onkeypress="return noreturn(event);"> Nein
-				<input type='radio' name='sc[zauber][]' value="1" size='30' onkeypress="return noreturn(event);"> Ja
+				<input class="form-control" type='radio' name='sc[zauber][]' value="0" checked="checked" size='30' onkeypress="return noreturn(event);"> Nein
+				<input class="form-control" type='radio' name='sc[zauber][]' value="1" size='30' onkeypress="return noreturn(event);"> Ja
 			</div><br>
 			<div class='header' style='position: absolute'><b>Con-Tage<br /> (Charakter)</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input id='contage' type='text' name='sc[contage]' size='30' onkeypress="return noreturn(event);">
+				<input class="form-control" id='contage' type='text' name='sc[contage]' size='30' onkeypress="return noreturn(event);">
 			</div><br>														
 		</div>
 		<div id="boxnsc" style="display:none">
@@ -310,34 +310,34 @@ $nscBan = ($nscRest <= '0') ? true : false;
 			<br />
 			<div class='header' style='position: absolute'><b>Festrolle<br /> (plotrelevant)</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input type='checkbox' name='nsc[festrolle_plot]' value="1" size='30' onkeypress="return noreturn(event);">
+				<input class="form-control" type='checkbox' name='nsc[festrolle_plot]' value="1" size='30' onkeypress="return noreturn(event);">
 			</div><br>			
 			<div class='header' style='position: absolute'><b>Festrolle<br /> (Ambiente)</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input type='checkbox' name='nsc[festrolle_ambiente]' value="1" size='30' onkeypress="return noreturn(event);">
+				<input class="form-control" type='checkbox' name='nsc[festrolle_ambiente]' value="1" size='30' onkeypress="return noreturn(event);">
 			</div><br>
 			<div class='header' style='position: absolute'><b>Springer</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input type='checkbox' name='nsc[springer]' value="1" size='30' onkeypress="return noreturn(event);">
+				<input class="form-control" type='checkbox' name='nsc[springer]' value="1" size='30' onkeypress="return noreturn(event);">
 			</div><br>
 			<div class='header' style='position: absolute'><b>Tr&auml;ume</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input type='checkbox' name='nsc[traeume]' value="1" size='30' onkeypress="return noreturn(event);">
+				<input class="form-control" type='checkbox' name='nsc[traeume]' value="1" size='30' onkeypress="return noreturn(event);">
 			</div><br>
 			<div class='header' style='position: absolute'><b>Ich kann schminken:</b></div>
 			<div style='position: relative; padding-left: 180px'>
-				<input type='radio' name='nsc[schminken]' value="0" size='30' checked="checked" onkeypress="return noreturn(event);"> Nein
-				<input type='radio' name='nsc[schminken]' value="1" size='30' onkeypress="return noreturn(event);"> Ja
+				<input class="form-control" type='radio' name='nsc[schminken]' value="0" size='30' checked="checked" onkeypress="return noreturn(event);"> Nein
+				<input class="form-control" type='radio' name='nsc[schminken]' value="1" size='30' onkeypress="return noreturn(event);"> Ja
 			</div><br>
 			<div class='header' style='position: absolute'><b>Ich k&auml;mpfe:</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input type='radio' name='nsc[kaempfen]' value="0" size='30' checked="checked" onkeypress="return noreturn(event);"> Ungern
-				<input type='radio' name='nsc[kaempfen]' value="1" size='30' onkeypress="return noreturn(event);"> Gerne
+				<input class="form-control" type='radio' name='nsc[kaempfen]' value="0" size='30' checked="checked" onkeypress="return noreturn(event);"> Ungern
+				<input class="form-control" type='radio' name='nsc[kaempfen]' value="1" size='30' onkeypress="return noreturn(event);"> Gerne
 			</div><br>
 			<div class='header' style='position: absolute'><b>Ich zaubere:</b></div>
 			<div style='position: relative; padding-left: 140px'>
-				<input type='radio' name='nsc[zaubern]' value="0" size='30' checked="checked" onkeypress="return noreturn(event);"> Ungern
-				<input type='radio' name='nsc[zaubern]' value="1" size='30' onkeypress="return noreturn(event);"> Gern
+				<input class="form-control" type='radio' name='nsc[zaubern]' value="0" size='30' checked="checked" onkeypress="return noreturn(event);"> Ungern
+				<input class="form-control" type='radio' name='nsc[zaubern]' value="1" size='30' onkeypress="return noreturn(event);"> Gern
 			</div><br>
 		</div>
 		<br />		
@@ -349,18 +349,18 @@ $nscBan = ($nscRest <= '0') ? true : false;
 				<b>Bemerkung</b>
 			</div>
 			<div>
-				<textarea cols='30' rows='5' name='member[bemerkung]'><? echo $_SESSION['c5']['member']['bemerkung']?></textarea>
+				<textarea class="form-control" cols='30' rows='5' name='member[bemerkung]'><? echo $_SESSION['c5']['member']['bemerkung']?></textarea>
 			</div><br />
 			<div>
 				<b>Mein Name auf der HP</b>&nbsp;<img src='xcms/views/images/icons/help.png' onmouseover="Tip('Dein Name erscheint in der<br> Teilnehmerliste auf der Homepage')" onmouseout="UnTip()">
-				 &nbsp;<input type='checkbox' name='member[sichtbar]' value='1' class='radio' <? if (isset($_SESSION['c5']['member']['sichtbar'])) echo ($_SESSION['c5']['member']['sichtbar'] == '1')? 'checked' : ''; else echo 'checked';?>>
+				 &nbsp;<input class="form-control" type='checkbox' name='member[sichtbar]' value='1' class='radio' <? if (isset($_SESSION['c5']['member']['sichtbar'])) echo ($_SESSION['c5']['member']['sichtbar'] == '1')? 'checked' : ''; else echo 'checked';?>>
 			</div><br><br>
 			<div class='header'>
 				<b>Ich habe die <a href='?action=agb' target=_blank>AGB</a> gelesen und akzeptiert.</b>
-				<input type='checkbox' name='agb' value='true' id='agb' onclick='toggle()' class='radio'>
+				<input class="form-control" type='checkbox' name='agb' value='true' id='agb' onclick='toggle()' class='radio'>
 			</div>
 			<br>
-			<input type='submit' value=' Anmelden ' disabled="disabled" id='submit'></div>
+			<input  type='submit' value=' Anmelden ' disabled="disabled" id='submit'></div>
 		</form>
 	</div>	
 	<div>(Mit <b>*</b> gekennzeichnete Felder sind Pflichtfelder)</span>
