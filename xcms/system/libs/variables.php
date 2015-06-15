8abbcb60035b46a27c9190993d637e5c51b97122
bknobloch <serrin@posteo.de>
2015-06-15 13:09:17 +0200

    Feld Charaktername war bei SC doppelt. Ist nun nur noch 1x vorhanden

---
+++ xcms/system/libs/variables.php
@@ -0,0 +1,11 @@
+<?php 
+
+define('ADMIN', 'test');
+define('ADMINPW', 'test123');
+
+	$loc = "localhost";
+	$database = "db1115355-anmeldung";
+	$user = "db1115355-anm";
+	$pass = "4nM3ldung";
+
+?>
