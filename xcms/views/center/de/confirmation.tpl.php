72523e95c209e0aa8e1ea491b35b1a4fc7f18626
Bernd Knobloch <serrin@posteo.de>
2015-07-15 00:41:06 +0200

    Bestätigungsseite hinzugefügt

---
+++ xcms/views/center/de/confirmation.tpl.php
@@ -0,0 +1,8 @@
+<div class="container text-center">
+<p>Du hast dich erfolgreich für das Con angemeldet!</p>
+<p>Bitte überweise nun Deinen Teilnahmebeitrag auf das unter <a target="_blank" href="jetland.dreywassern.de">jetland.dreywassern.de</a> angegebene Konto.</p>
+<p>Bitte beachte, dass wir deine Anmeldung erst dann weiter bearbeiten, wenn dein Beitrag bei uns eingegangen ist.</p>
+<form class="form-horizontal" role="form">
+	<button type='button' class="btn btn-success" onClick="self.location.href = '<? echo $_SERVER['PHP_SELF']?>?action=teilnehmer'">OK</input>
+</form>
+</div>
