5b044b7b5a95f242e2d48e33f5f37d24336ace24
Bernd Knobloch <serrin@posteo.de>
2015-07-17 22:31:29 +0200

    Responsive Layout der Bestätigungsseite korrigiert

--- xcms/views/center/de/confirmation.tpl.php
+++ xcms/views/center/de/confirmation.tpl.php
@@ -3,7 +3,7 @@
 /* About */
 /*    background: #e4e4e4;*/
 #confirmation{
-	background: rgba(0,0,0,0.8);
+	background: rgba(0,0,0,0.7);
 }
 
 #confirmation h1,
@@ -17,24 +17,14 @@
 
 
 .confirmation-box-container {
-	margin: 60px 0;
+	margin: 20px 0;
 	text-align: center;
 	padding-bottom: 20px;
-}
-
-.confirmation-box-container:last-child{
-	margin-bottom: 0;
 }
 
 .confirmation-box {
 	text-align: left;
 	background: rgba(255,255,255,0.2);
-}
-
-.confirmation-box .icon i {
-	font-size: 35px;
-	padding-top: 12px;
-	display: block;
 }
 
 .confirmation-box .description {
@@ -57,40 +47,30 @@
 <!-- About -->
 <section id="confirmation" class="section">
 	<div class="container">
-		<div class="row">
-			<div class="col-sm-12 center">
+		<div class="confirmation-box-container">
+			<div class="row">
 				<h3 class="confirmation-title">Du hast dich erfolgreich für das Con angemeldet.</h3>
-				<div class="confirmation-title-border"></div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-12 confirmation-box-container">
+			<div class="row">
 				<div class="description">
 					<p>Vielen Dank für Deine Anmeldung. Wir freuen uns auf Dich!</p>
 					<p>Bitte überweise nun Deinen Teilnahmebeitrag auf das folgende Konto.</p>
-				</div>
-				<div class="row">
-					<div class="col-sm-12 confirmation-box-container">
-						<div class="col-sm-4"></div>
-						<div class="col-sm-4 confirmation-box">
+					<div class="row">
+						<div class="col-xs-1 col-md-4"></div>
+						<div class="confirmation-box col-xs-10 col-md-4">
 							<p><b>Inhaber:</b> Christoph Platt<br/>
 								<b>Institut:</b> Sparkasse München<br/>
 								<b>IBAN:</b> DE26 7015 0000 1000 5296 75<br/>
 								<b>BIC:</b> SSKMDEMMXXX<br/>
-								<b>Verwendungszweck:</b> &lt;SC oder NSC&gt;, J11, &lt;Realname&gt;</p>
+								<b>Verwendungszweck:</b> &lt;SC oder NSC&gt;, J11, &lt;Realname&gt;
+							</p>
 						</div>
-						<div class="col-sm-4"></div>
+						<div class="col-xs-1 col-md-4"></div>
 					</div>
-					<div class="row">
-						<div class="col-sm-12 confirmation-box-container">
-							<div class="col-sm-12">
-								<p>Beachte bitte außerdem, dass wir Deine Anmeldung erst dann weiter bearbeiten, wenn Dein Beitrag bei uns eingegangen ist.</p>
-								<form class="form-horizontal" role="form">
-									<button type='button' class="btn btn-success" onClick="self.location.href = '<? echo $_SERVER['PHP_SELF']?>?action=teilnehmer'">OK</input>
-								</form>
-							</div>
-						</div>
-					</div>
+					<p>Beachte bitte außerdem, dass wir Deine Anmeldung erst dann weiter bearbeiten, wenn Dein Beitrag bei uns eingegangen ist.</p>
+					<form class="form-horizontal" role="form">
+						<button type='button' class="btn btn-success" onClick="self.location.href = '<? echo $_SERVER['PHP_SELF']?>?action=teilnehmer'">OK</input>
+					</form>
 				</div>
 			</div>
 		</div>
